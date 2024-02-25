aa197ef191ee3ab52e80bbcf77968a1bd94dd225
sergiojimenezdos <sergiojimenezjimenez@protonmail.com>
2024-02-25 17:32:45 +0100

    buid: Change database connection to SQL Server

--- projectef/Program.cs
+++ projectef/Program.cs
@@ -4,7 +4,12 @@ using projectef;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+// Set connection to Database in Memory
+//builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
+// Set connection to SQL Server
+builder.Services.AddSqlServer<TasksContext>("Data Source=(local); Initial Catalog= TasksDB;Trusted_Connection=True; Integrated Security=True;TrustServerCertificate=True");
+
 
 var app = builder.Build();
 
