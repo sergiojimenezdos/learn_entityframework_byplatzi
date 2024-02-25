407cc80eb4228adbfd97510f068d76b9c61af086
sergiojimenezdos <sergiojimenezjimenez@protonmail.com>
2024-02-25 13:20:04 +0100

    feat: Add dbconnection endpoint

--- projectef/Program.cs
+++ projectef/Program.cs
@@ -10,4 +10,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/dbconnection", async([FromServices] TasksContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database in memory: " + dbContext.Database.IsInMemory());
+});
+
 app.Run();
