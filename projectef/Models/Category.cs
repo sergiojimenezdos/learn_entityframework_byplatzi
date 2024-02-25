db91710e24a458d6ec9dc85ad754085747663f33
sergiojimenezdos <sergiojimenezjimenez@protonmail.com>
2024-02-25 13:07:31 +0100

    build: Add DataAnnotatios to models

--- projectef/Models/Category.cs
+++ projectef/Models/Category.cs
@@ -1,11 +1,19 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace projectef.Models;
 
+[Table("Category")]
 public class Category
 {
+    [Key]
     public Guid CategoryId {get; set;}
+
+    [Required]
+    [MaxLength(150)]
     public string Name {get; set;}     
+
     public string Description {get; set;}    
+
     public virtual ICollection<Task> Tasks {get; set;}     
 }
