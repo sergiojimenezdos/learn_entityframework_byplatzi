e9455846b590568db6c969c5ccaf9e60ec039685
sergiojimenezdos <sergiojimenezjimenez@protonmail.com>
2024-02-25 13:19:23 +0100

    build: Add DbContext InMemory to Program

--- projectef/Program.cs
+++ projectef/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
