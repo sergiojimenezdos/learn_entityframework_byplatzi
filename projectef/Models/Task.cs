705caf73a1cc9d0cd9562cf4c416925bbb0f75a2
sergiojimenezdos <sergiojimenezjimenez@protonmail.com>
2024-02-26 07:18:36 +0100

    build: Set FluentAPI config instead of DataAnnotations

--- projectef/Models/Task.cs
+++ projectef/Models/Task.cs
@@ -1,30 +1,14 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace projectef.Models;
 
-[Table("Task")]
 public class Task
 {
-    [Key]
     public Guid TaskId {get; set;}
-
-    [ForeignKey("CategoryId")]
     public Guid CategoryId {get; set;}
-
-    [Required]
-    [MaxLength(200)]
     public string Title {get; set;}     
-
     public string Description {get; set;}     
-
     public Priority Priority {get; set;}     
-
     public DateTime CreationDate {get; set;}     
-
     public virtual Category Category{get; set;}    
-
-    [NotMapped]
     public string Summary{get;set;}
 }
 
--- projectef/TasksContext.cs
+++ projectef/TasksContext.cs
@@ -8,4 +8,27 @@ public class TasksContext: DbContext
     public DbSet<Category> Categories {get;set;}
     public DbSet<Models.Task> Tasks{get;set;}
     public TasksContext(DbContextOptions<TasksContext> options): base(options){}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(category =>
+        {
+            category.ToTable("Category");
+            category.HasKey(k => k.CategoryId);
+            category.Property(p => p.Name).IsRequired().HasMaxLength(150);
+            category.Property(p => p.Description);
+        });
+
+        modelBuilder.Entity<Models.Task>(t =>
+        {
+            t.ToTable("Task");
+            t.HasKey(p => p.TaskId);
+            t.HasOne(p => p.Category).WithMany(p => p.Tasks).HasForeignKey(p => p.CategoryId);
+            t.Property(p => p.Title).IsRequired().HasMaxLength(200);
+            t.Property(p => p.Description);
+            t.Property(p => p.Priority);
+            t.Property(p => p.CreationDate);
+            t.Ignore(p => p.Summary);
+        });
+    }
 }
